3693c88712b7ff5b130db245957761d08b2e2b2f
Danish <danishpinto102@gmail.com>
2023-10-25 05:57:16 +0530

    auth error fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -46,70 +46,70 @@ export const authOptions: NextAuthOptions = {
     ],
     callbacks: {
 
-        // async jwt({ token, user, session }) {
-        //     // console.log("JWT callback: ", { token, user, session });
+        async jwt({ token, user, session }) {
+            // console.log("JWT callback: ", { token, user, session });
 
-        //     // pass in the id to the token to be returned. This will be used by the async session
-        //     if (user) {
-        //         return {
-        //             ...token,
-        //             id: user.id,
-        //         }
-        //     }
-
-        //     return token;
-        // },
-
-        // async session({ session, token, user }) {
-        //     // console.log("Session callback: ", { token, user, session });
-
-        //     // pass in userId to the session
-        //     return {
-        //         ...session,
-        //         user: {
-        //             ...session.user,
-        //             id: token.id,
-        //         }
-        //     };
-
-        //     return session;
-        // },
-
-        async jwt({ token, user }) {
-            // const dbUser = (await db.get(`user:${token.id}`)) as User | null
-            const dbUserResult = await fetchRedis('get', `user:${token.id}`) as
-                | string
-                | null;
-
-
-            if (!dbUserResult) {
-                if (user) {
-                    token.id = user!.id
+            // pass in the id to the token to be returned. This will be used by the async session
+            if (user) {
+                return {
+                    ...token,
+                    id: user.id,
                 }
-
-                return token;
             }
 
-            const dbUser = JSON.parse(dbUserResult) as User;
-
-            return {
-                id: dbUser.id,
-                name: dbUser.name,
-                email: dbUser.email,
-                picture: dbUser.image,
-            }
+            return token;
         },
 
-        async session({ session, token }) {
-            if (token) {
-                session.user.id = token.id
-                session.user.name = token.name
-                session.user.email = token.email
-                session.user.image = token.picture
-            }
+        async session({ session, token, user }) {
+            // console.log("Session callback: ", { token, user, session });
+
+            // pass in userId to the session
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id,
+                }
+            };
 
             return session;
         },
+
+        // async jwt({ token, user }) {
+        //     // const dbUser = (await db.get(`user:${token.id}`)) as User | null
+        //     const dbUserResult = await fetchRedis('get', `user:${token.id}`) as
+        //         | string
+        //         | null;
+
+
+        //     if (!dbUserResult) {
+        //         if (user) {
+        //             token.id = user!.id
+        //         }
+
+        //         return token;
+        //     }
+
+        //     const dbUser = JSON.parse(dbUserResult) as User;
+
+        //     return {
+        //         id: dbUser.id,
+        //         name: dbUser.name,
+        //         email: dbUser.email,
+        //         picture: dbUser.image,
+        //     }
+        // },
+
+        // async session({ session, token }) {
+        //     if (token) {
+        //         session.user.id = token.id
+        //         session.user.name = token.name
+        //         session.user.email = token.email
+        //         session.user.image = token.picture
+        //     }
+
+        //     return session;
+        // },
 
         redirect() { // redirect when the user has signed in
             return '/dashboard'
