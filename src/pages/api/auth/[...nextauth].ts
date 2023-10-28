21e9ce38c0e3612ce719e00376b633cd2aa49337
Danish <danishpinto102@gmail.com>
2023-10-28 13:42:55 +0530

    nextauth error resolved

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
 import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth/next";
 
-export default NextAuth(authOptions)
+// export default NextAuth(authOptions)
 
-// const authHandler = NextAuth(authOptions);
-// export default async function handler(...params: any[]) {
-//     await authHandler(...params);
-// }
+const authHandler = NextAuth(authOptions);
+export default async function handler(...params: any[]) {
+    await authHandler(...params);
+}
